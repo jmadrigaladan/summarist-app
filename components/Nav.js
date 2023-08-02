ff5eafe7a47b5dcda089288482dd1e715c65ed50
jmadrigaladan <jorgemadrigaladan@gmail.com>
2023-08-01 21:46:25 -0700

    working on landing styling

--- components/Nav.js
+++ components/Nav.js
@@ -1,20 +1,23 @@
 export default function Nav() {
   return (
     <nav className="h-[80px]">
-      {/* nav__wrapper  */}
       <div className="flex justify-between items-center max-w-[1070px] w-full h-full mx-auto px-[24px]">
-        {/* nav__img--mask */}
         <figure className="max-w-[200px]">
-          {/* nav__img */}
-          <img className="w-full h-full" src="" alt="logo" />
+          <img className="w-full h-full" src={"/assets/logo.png"} alt="logo" />
         </figure>
-        {/* nav__list--wrapper */}
         <ul className="flex gap-[24px]">
-            {/* nav__list */}
-          <li className="text-[#032b41] hover:text-[#2bd97c] cursor-pointer transition duration-100">Login</li>
-          <li className="cursor-not-allowed">About</li>
-          <li className="nav__list nav__list--mobile">Contact</li>
-          <li className="max-[576px]:hidden">Help</li>
+          <li className="text-[#032b41] hover:text-[#2bd97c] cursor-pointer transition duration-100">
+            Login
+          </li>
+          <li className="text-[#032b41] max-[576px]:hidden cursor-not-allowed">
+            About
+          </li>
+          <li className="text-[#032b41] max-[576px]:hidden cursor-not-allowed">
+            Contact
+          </li>
+          <li className="text-[#032b41] max-[576px]:hidden cursor-not-allowed">
+            Help
+          </li>
         </ul>
       </div>
     </nav>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Landing from "@/components/Landing";
 import Nav from "@/components/Nav";
 
 export default function Home() {
   return (
     <div>
       <Nav />
+      <Landing />
     </div>
   );
 }
