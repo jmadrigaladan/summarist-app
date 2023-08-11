f29203510068bf36b0cdcdf97b62e7df97f2388b
jmadrigaladan <jorgemadrigaladan@gmail.com>
2023-08-10 17:52:33 -0700

    modal almost complete missing close icon

--- components/modals/LogInModal.js
+++ components/modals/LogInModal.js
@@ -1,6 +1,7 @@
 import { closeLogInModal, openLogInModal } from "@/redux/modalSlice";
 import Modal from "@mui/material/Modal";
 import { useDispatch, useSelector } from "react-redux";
+import { BiSolidUser } from "react-icons/bi";
 
 export default function LogInModal() {
   const isOpen = useSelector((state) => state.modals.loginModalOpen);
@@ -17,13 +18,68 @@ export default function LogInModal() {
       <Modal
         open={isOpen}
         onClose={() => dispatch(closeLogInModal())}
-        className="flex justify-center items-center"
+        disableAutoFocus={true}
+        className="flex justify-center items-center outline-none "
       >
-        <div className="bg-white w-[400px] h-[300px] rounded-lg flex justify-center">
-          <div className="w-full justify-center">
-            <h1 className="text-center pt-[48px] px-[32px] pb-[24px]">
-              Log In to Summarist
+        <div className="bg-white w-[400px]  rounded-lg flex flex-col justify-center">
+          <div className="w-full flex flex-col items-center pt-[48px] px-[32px] pb-[24px]">
+            <h1 className="text-center text-[20px] font-bold mb-[24px] text-[#032b41] ">
+              Log in to Summarist
             </h1>
+            <div className="flex justify-center w-full">
+              <button className="flex items-center  bg-[#3a579d] w-full h-[40px] min-w-[180px] rounded-[4px]">
+                <BiSolidUser className="w-[40px] h-[40px] py-[4px] mr-[64px] fill-white" />
+                <div className="text-white text-center">Login as a Guest</div>
+              </button>
+            </div>
+            <div className="my-[16px] flex items-center justify-between w-full">
+              <hr className="w-[40%] h-[8px] text-[#bac8ce]" />
+              or
+              <hr className="w-[40%] h-[8px] text-[#bac8ce]" />
+            </div>
+            <div className="flex justify-center w-full">
+              <button className="flex items-center bg-[#4285f4] w-full h-[40px] min-w-[180px] rounded-[4px]">
+                <figure className="pl-[4px]">
+                  <img
+                    src="/assets/google.png"
+                    alt=""
+                    className="w-[36px] h-[36px] bg-white mr-[60px] rounded-[4px] "
+                  />
+                </figure>
+                <div className="text-white">Login with Google</div>
+              </button>
+            </div>
+            <div className="my-[16px] flex items-center justify-between w-full">
+              <hr className="w-[40%] h-[8px] text-[#bac8ce]" />
+              or
+              <hr className="w-[40%] h-[8px] text-[#bac8ce]" />
+            </div>
+
+            <div className="w-full ">
+              <input
+                className="w-full h-[40px] rounded-md bg-transparent border-[2px] border-[#bac8ce] p-4 focus:outline-none focus:border-[#2bd97c]"
+                placeholder="Email Address"
+                type="email"
+                onChange={() => {}}
+              />
+              <input
+                className="w-full h-[40px] mt-[16px] rounded-md bg-transparent border-[2px] border-[#bac8ce] p-4 focus:outline-none focus:border-[#2bd97c]"
+                placeholder="Password"
+                type="password"
+                onChange={() => {}}
+              />
+            </div>
+
+            <button className="bg-[#2bd97c] w-full rounded-md h-[40px] mt-[16px]">
+              LogIn
+            </button>
+
+            <div className="w-full text-center text-[#116be9] text-[14px] font-light mt-[24px] cursor-pointer">
+              Forgot your password?
+            </div>
+          </div>
+          <div className="flex items-center justify-center w-full bg-[#f1f6f4] h-[40px] text-[#116be9] text-[14px] font-light cursor-pointer ">
+            Don't have an account?
           </div>
         </div>
       </Modal>
