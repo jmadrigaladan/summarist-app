d576df5b2c5f64e785d4a005941edf1a4e11697a
jmadrigaladan <jorgemadrigaladan@gmail.com>
2023-09-07 15:51:39 -0700

    fixed files

--- firebase.js
+++ firebase.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAsFIMGNKnU-ORPRL2M3sW-S5NF41TCBVE",
-  authDomain: "summarist-app.firebaseapp.com",
-  projectId: "summarist-app",
-  storageBucket: "summarist-app.appspot.com",
-  messagingSenderId: "91431535767",
-  appId: "1:91431535767:web:7d9c9a844a2df70bfdd5d2",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
